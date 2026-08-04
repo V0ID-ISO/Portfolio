db4ee12edfe6de0dcd8ca9f8d2696914bb2a0eca
V0ID-ISO <jkjob2008@gmail.com>
2026-08-03 23:27:08 -0400

    Update data.js

--- data.js
+++ data.js
@@ -226,12 +226,6 @@ const CERTIFICATIONS = [
     date: "2025",
     credentialUrl: "#"
   } 
-   {
-    name: "Cybersecurity",
-    issuer: "Infomation technology Specialist",
-    date: "2025",
-    credentialUrl: "#"
-  }
   // Add more the same way. Set credentialUrl to "#" if you don't have a
   // public verification link yet.
 ];
